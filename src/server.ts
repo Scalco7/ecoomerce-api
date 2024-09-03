a6e20e22832afbd8725058afa3e93fc858d30c30
Scalco7 <fscalco7@gmail.com>
2024-09-03 18:21:48 -0300

    basic server ts

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import { Router } from 'express'
+
+// import dotenv from 'dotenv'
+// import cors from 'cors';
+
+// dotenv.config()
+const app = express()
+const route = Router()
+
+app.use(express.json())
+// app.use(cors())
+
+app.use(route)
+app.listen(process.env.PORT, () => console.log("heyy, api is listen on port: " + process.env.PORT))
