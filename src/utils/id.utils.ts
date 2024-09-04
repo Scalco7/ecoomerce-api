5d92cb9e498f4c8825067a40dab1c075f581698d
Scalco7 <fscalco7@gmail.com>
2024-09-04 16:07:04 -0300

    random ID

---
+++ src/utils/id.utils.ts
@@ -0,0 +1,11 @@
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+export function generateRandomID(): string {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < 8; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
