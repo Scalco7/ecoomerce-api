6fbed3b0495373fe6b3f886cf1caff6af5fc2f88
Scalco7 <fscalco7@gmail.com>
2024-09-05 19:28:26 -0300

    del unusuble

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import { Router, Request, Response } from 'express'
 
 import dotenv from 'dotenv'
 import cors from 'cors';
@@ -7,15 +6,9 @@ import { productsRoute } from './routes/products.route';
 
 dotenv.config()
 const app = express()
-const route = Router()
 
 app.use(express.json())
 app.use(cors())
 
-route.get('test', (req: Request, res: Response) => {
-    res.status(200).json({ message: "Deu certo" })
-})
-
 app.use("/products", productsRoute)
-app.use(route)
 app.listen(process.env.PORT, () => console.log("heyy, api is listen on port: " + process.env.PORT))
