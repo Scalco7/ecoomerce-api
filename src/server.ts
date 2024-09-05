d4e34fd8478afba102d68ed9d220555899bf474c
Scalco7 <fscalco7@gmail.com>
2024-09-05 11:36:34 -0300

    teste route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 
 import dotenv from 'dotenv'
 import cors from 'cors';
@@ -12,5 +12,10 @@ const route = Router()
 app.use(express.json())
 app.use(cors())
 
+route.get('test', (req: Request, res: Response) => {
+    res.status(200).json({ message: "Deu certo" })
+})
+
 app.use("/products", productsRoute)
+app.use(route)
 app.listen(process.env.PORT, () => console.log("heyy, api is listen on port: " + process.env.PORT))
